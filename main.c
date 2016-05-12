d678abe1d64876f18ba3193132883d9475d62e0c
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-12 21:10:09 +0000

    Put resp function contents into main function, deleted resp.

--- main.c
+++ main.c
@@ -5,41 +5,33 @@
 #include <stdio.h>
 #include "header.h"
 
-int main(int argc, char **argv, char **env) {
-        int err;
-
-        if (argc != 3) {
-                return (1);
-        }
-        err = resp(argv[1], argv[2], env);
-        if (err == 1) {
-                return (1);
-        }
-        return (0);
-}
-
 /*
- * resp() - Takes arguments from command line and locates them in PATH.
+ * Takes arguments from command line and locates them in PATH.
  * @cmd: The command entered by the user.
  * @str: The string entered by the user.
  * @env: The environment.
  *
  * Return: 0 on success, 1 on failure.
  */
-int resp(char *cmd, char *str, char **env)
-{
+
+int main(int argc, char **argv, char **env) {
+
+	char *path;
         pid_t pid;
-        char *path;
         char *exec_argv[] = {NULL, NULL, NULL};
         int status;
 
-        path = get_path(cmd);
-        exec_argv[0] = path;
-        exec_argv[1] = str;
+        if (argc != 3) {
+                return (1);
+        }
+
+        path = get_path(argv[1]);
+	exec_argv[0] = path;
+        exec_argv[1] = argv[2];
 
         /* Test */
-        printf("the string is: %s\n", str);
-        printf("the command is: %s\n", cmd);
+        printf("the string is: %s\n", argv[2]);
+        printf("the command is: %s\n", argv[1]);
 
         if ((pid = fork()) == -1) {
                 perror("fork");
@@ -50,6 +42,7 @@ int resp(char *cmd, char *str, char **env)
                 wait(&status);
                 printf("Child process terminated.\n");
         }
+
         return(0);
 }
 
