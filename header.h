15cf00a5339f80ed599eee75e4122de4a7eb0e44
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-12 22:41:52 +0000

    added str_len function code

--- header.h
+++ header.h
@@ -1,4 +1,3 @@
 int print_char(char);
-int resp(char *, char *, char **);
-char *cmd_with_path(char *);
 char *concat_strings(char *s1, char *s2);
+int str_len(char *str);
--- main.c
+++ main.c
@@ -94,3 +94,18 @@ char *concat_strings(char *s1, char *s2)
 
         return p;
 }
+
+int str_len(char *str)
+{
+  int i;			/* i used as a counter */
+
+  i = 0;			/* initialize at 0 */
+
+  while (*str != '\0') 		/* while string isn't over */
+    {
+      i++;			/* increase counter */
+      str++;			/* pointer arithmetic for next char */
+    }
+
+  return i;
+}
