e5e08876a1f0a7c0be27501ffe00d555a712ac0a
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-05-13 13:23:09 -0700

    Add function to find location of program within PATH variable.

--- header.h
+++ header.h
@@ -1,4 +1,6 @@
 int print_char(char);
 char *concat_strings(char *s1, char *s2);
 int str_len(char *str);
+int strcmp(char *, char *);
 int *print_prompt();
+char *find_path(char *, char **);
--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "header.h"
+#include <dirent.h>
 #include "libshell/libshell.h"
 
 /*
@@ -14,13 +15,13 @@
  *
  * Return: 0 on success, 1 on failure.
  */
-
 int main(int argc, __attribute__((unused)) char **argv, char **env) {
 
         pid_t pid;
         int status;
         char c;
-        char *str;
+        char *path_to_exec;
+        char *raw_str;
         char **arr;
         int i = 0;
         char *exec_argv[] = {NULL, NULL, NULL};
@@ -29,16 +30,25 @@ int main(int argc, __attribute__((unused)) char **argv, char **env) {
 		return 1;
         }
 
-        str = malloc( sizeof(char) * 10 );
+        raw_str = malloc(sizeof(char) * (100 + 1));
 
         print_prompt();
 
         while(read(0, &c, 1)) {
                 if(c == '\n') {
-                        str[i] = '\0';
+                        raw_str[i] = '\0';
                         i = 0;
-                        arr = string_split(str, ' ');
-                        exec_argv[0] = concat_strings("/bin/", arr[0]);
+                        /* Delete */
+                        arr = string_split(raw_str, ' ');
+
+                        path_to_exec = find_path(arr[0], env);
+                        path_to_exec = concat_strings(path_to_exec, "/");
+
+                        printf("The path to the exec is: %s\n", path_to_exec);
+
+                        exec_argv[0] = concat_strings(path_to_exec, arr[0]);
+
+                        printf("exec_argv[0]: %s\n", exec_argv[0]);
                         exec_argv[1] = arr[1];
 
                         if ((pid = fork()) == -1) {
@@ -52,10 +62,48 @@ int main(int argc, __attribute__((unused)) char **argv, char **env) {
                         print_prompt();
                         read(0, &c, 1);
                 }
-                str[i] = c;
+                raw_str[i] = c;
                 ++i;
         }
         return 0;
+}
+
+char *find_path(char *command, char **env) {
+        DIR *dir;
+        struct dirent *dir_search;
+        char *path;
+        char **path_arr;
+        int i;
+
+        dir_search = malloc(sizeof(struct dirent));
+        path = malloc(sizeof(char) * (100 + 1));
+
+        /* Take the path variable and isolate it into a variable.
+         * PATH is the 8th element in the env array. The first absolute
+         * path begins at five chars in, hence [i + 5].
+         */
+        for(i = 0; env[8][i + 5] != '\0'; i++) {
+                path[i] = env[8][i + 5];
+        }
+
+        /* Split the path into an array for searching. */
+        path_arr = string_split(path, ':');
+
+        /* Increment through the path array to find the location of the program. */
+        for(i = 0; path_arr != NULL; ++i) {
+
+                /* Open each directory. */
+                dir = opendir(path_arr[i]);
+
+                /* Seach this directory for the program name. */
+                while((dir_search = readdir(dir)) != NULL) {
+                        if (strcmp(dir_search->d_name, command) == 0) {
+                                printf("Found the program %s, in the directory %s\n", dir_search->d_name, path_arr[i]);
+                                break;
+                        }
+                }
+        }
+        return path_arr[i];
 }
 
 /*
@@ -119,4 +167,19 @@ int *print_prompt(){
                 ++i;
         }
         return 0;
+}
+
+int strcmp(char *s1, char *s2)
+{
+  int i = 0;
+  int j = 0;
+  for ( ; s1[i] != '\0'; i++)
+  {
+  	if (s1[j] != s2[j]) /* if chars are different, break */
+	  {
+	    break;
+	  }
+	  j++;
+  }
+  return(s1[j] - s2[j]); /* return difference in chars */
 }
