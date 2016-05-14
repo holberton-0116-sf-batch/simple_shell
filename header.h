93c289513b17dcb9a97510840b2975c0445a27fb
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-05-14 16:42:46 -0700

    Remove variable names from prototypes.

--- header.h
+++ header.h
@@ -1,9 +1,9 @@
 int print_char(char);
-char *concat_strings(char *s1, char *s2);
-int str_len(char *str);
+char *concat_strings(char *, char *);
+int str_len(char *);
 int strcmp(char *, char *);
 int *print_prompt();
 void print_env(char **);
 char *find_path(char *, char **);
-int ch_dir(char *cmd);
-char *get_env_var(char *var, char **env);
+int ch_dir(char *);
+char *get_env_var(char *, char **);
